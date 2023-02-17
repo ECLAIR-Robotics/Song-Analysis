ef710534baab365d113ba553767d9541baae48ea
sarahwz <120886534+sarahwz@users.noreply.github.com>
2023-02-16 19:31:05 -0600

    spotipy

--- lastfm.py
+++ lastfm.py
@@ -15,11 +15,12 @@ params = {
 }
 
 url = 'http://ws.audioscrobbler.com/2.0/'
-response = requests.get(url, headers=headers, params=params)
+#response = requests.get(url, headers=headers, params=params)
 response = requests.get('https://2.0/?method=track.getTags&api_key=611bb642df86b5ac99ab8ec251e59641&artist=AC/DC&track=Hells+Bells&use...', headers=headers, params=params)
-data = response.json()
+#data = response.json()
+print(response)
 
-if response.status_code == 200:
+"""if response.status_code == 200:
     if 'tags' in data:
         tags = [item['name'] for item in data['tags']['tag']]
         print("Tags for 'Imagine' by 'John Lennon'")
@@ -28,4 +29,4 @@ if response.status_code == 200:
     else:
         print("No tags found.")
 else:
-    print(f"Request failed with status code: {response.status_code}")
+    print(f"Request failed with status code: {response.status_code}")"""
---
+++ spotify.py
@@ -0,0 +1,15 @@
+import config
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(SpotifyOAuth(config.spotify_CLIENT_ID, config.spotify_CLIENT_SECRET, 'http://localhost/'))
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
