e4ea054079205eb7cf66239930a81240f0be4ef8
sarahwz <120886534+sarahwz@users.noreply.github.com>
2023-02-27 19:29:54 -0600

    stuff

--- dataSet.py
+++ dataSet.py
@@ -14,16 +14,9 @@ tqdm.pandas()
 df['track-artist'] = df['track'].apply(lambda i: [i])
 df['track-artist'] = df['track-artist'] + df['artist'].apply(lambda i: [i])
 
+
 df['track-artist'].progress_apply(lambda i: getLyrics(i.name, i[0], i[1]))
 
-# df['track-artist'] = df.apply(lambda i: (i.col_1, i.col_2))
-# df.apply(lambda i: getLyrics(i.col_1, i.col_2))
-print(df.head())
-# Array2d_result = np.genfromtxt(df, delimiter=",")
-# Array2d_result = df.to_numpy()
-# print(Array2d_result)
 
-# for i in range(9000):
-#     print(Array2d_result[i][0])
-#     print()
+print(df.head())
 
