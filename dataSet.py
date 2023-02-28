bbbe6642fa926daa71a15355ad390b4ccd177720
sarahwz <120886534+sarahwz@users.noreply.github.com>
2023-02-27 19:18:26 -0600

    added track-artist column

--- dataSet.py
+++ dataSet.py
@@ -3,12 +3,24 @@ import csv
 from lyricsgenius import Genius
 import pandas as pd
 import numpy as np
+import array
+
+from api import getLyrics
 
 df = pd.read_csv('muse_v3.csv')
-print(df)
-Array2d_result = np.genfromtxt(df, delimiter=",")
-Array2d_result = df.to_numpy()
-print(Array2d_result)
+df['track-artist'] = df['track'].apply(lambda i: [i])
+df['track-artist'] = df['track-artist'] + df['artist'].apply(lambda i: [i])
 
+df['track-artist'].apply(lambda i: getLyrics(i.name, i[0], i[1]))
 
+# df['track-artist'] = df.apply(lambda i: (i.col_1, i.col_2))
+# df.apply(lambda i: getLyrics(i.col_1, i.col_2))
+print(df.head())
+# Array2d_result = np.genfromtxt(df, delimiter=",")
+# Array2d_result = df.to_numpy()
+# print(Array2d_result)
+
+# for i in range(9000):
+#     print(Array2d_result[i][0])
+#     print()
 
