28c4ed7bf85ef98f11f1988ec99342caf9499390
Mehul Murali <mehul.murali@gmail.com>
2023-04-22 21:22:55 -0500

    kinda works

--- allsonglyrics.py
+++ allsonglyrics.py
@@ -1,19 +1,9 @@
-import api
+import playlist_info_2
 import os
 import requests
 from bs4 import BeautifulSoup
 import re
-
-def tester():
-    lis = "Smooth Criminal - 2012 Remaster"
-    pattern = re.compile('(- [^-]*)$')
-    lis = pattern.sub('', lis)
-    lis= re.sub(r'\([^]]*\)', '', lis)
-    lis = re.sub(r'\[[^]]*\]', '', lis)
-    lis = lis.strip()
-    print(lis)
-
-#tester()
+import pickle
 
 def isEnglish(s):
     if s is None:
@@ -22,7 +12,6 @@ def isEnglish(s):
         s.encode(encoding='utf-8').decode('ascii')
     except UnicodeDecodeError:
         return False
-    
     else:
         return True
         
@@ -36,51 +25,103 @@ def scrapeSong(title, artist):
     artist = artist.replace(" ", "+")
     baseURL = "https://sing.whatever.social/search?q="
     searchURL = f'{baseURL}{title}+{artist}'
+    
     r = requests.get(searchURL)
     soup = BeautifulSoup(r.content, 'html5lib')
-    songLink = soup.find('a', attrs = {'id':"search-item"})['href']
-    songLink = baseURL[:28]+songLink
+    songLink = soup.find('a', attrs = {'id':"search-item"}, href=True)['href'] if soup.find('a', attrs = {'id':"search-item"}, href=True) is not None else "skip"
+    if songLink != "skip":
+        songLink = baseURL[:28]+songLink
     
-    r = requests.get(songLink)
-    soup = BeautifulSoup(r.content, 'html5lib')
-    for br in soup('br'):
-        br.replace_with('\n')
-    lyricTable = soup.find('div', attrs = {'id':"lyrics"})
-    for row in lyricTable.findAll('a'):
-        lis = row.find('span').get_text()
-        lis = re.sub(r'\[[^]]*\]', '', lis)
-        if isEnglish(lis):
-         print(lis)
-
-
-scrapeSong("Walk Em Down (Don't Kill Civilians) [with 21 Savage & feat. Mustafa]", "Metro Boomin")
-
-
-
-
-
+        r = requests.get(songLink)
+        soup = BeautifulSoup(r.content, 'html5lib')
+        for br in soup('br'):
+            br.replace_with('\n')
+        lyricTable = soup.find('div', attrs = {'id':"lyrics"})
+        lyrics = ""
+        if lyricTable is not None:
+            for row in lyricTable.findAll('a'):
+                if(row.find('span') is not None):
+                    lis = row.find('span').get_text()
+                    lis = re.sub(r'\[[^]]*\]', '', lis)
+                    if isEnglish(lis):
+                        lyrics += lis +"\n"
+        return lyrics
+    return ''
+#print(scrapeSong("Someone Like You", "Adele"))
 def getLyrics():
     rootdir = os.getcwd() + "/Users"
-    userdict = {}
     for subdir, dirs, files in os.walk(rootdir):
         if(subdir != rootdir):
-            curList = []
+            lyrics = []
             for file in files:
                 with open(os.path.join(subdir, file)) as f:
                     for line in f.readlines():
                         title = line.split(';')[0]
                         title = re.sub(r'\([^]]*\)', '', title)
                         artist = line.split(';')[1].lstrip()
-                        #call api here
-                        lyricString = api.getLyrics(title, artist)
-                        if lyricString is not None:
-                            curList += lyricString.split(" ")                            
-            userdict[os.path.basename(os.path.normpath(subdir))] = curList
-    return userdict
+                        lyrics.append(scrapeSong(title, artist))  
+            with open(os.getcwd() + "/lyricPickles/" + os.path.basename(os.path.normpath(subdir))+ "_lyrics", 'wb') as f:
+                pickle.dump(lyrics, f)                                     
+    return 
 
-#getLyrics()
+def main():
+    userFile = open("users.txt",'r')
+    Lines = userFile.readlines()
+    for line in Lines:
+        playlist_info_2.main(line.strip())
+    getLyrics()
+main()
 
 
+## TRASH CODE ##
 
+# def getUrls():
+#      rootdir = os.getcwd() + "/Users"
+#      urlmatrix = []
+#      for subdir, dirs, files in os.walk(rootdir):
+#         if(subdir != rootdir):
+#             urls = []
+#             for file in files:
+#                 with open(os.path.join(subdir, file)) as f:
+#                     for line in f.readlines():
+#                         title = line.split(';')[0]
+#                         title = re.sub(r'\([^]]*\)', '', title)
+#                         artist = line.split(';')[1].lstrip()
+#                         pattern = re.compile('(- [^-]*)$') #gets rid of - Single Version, - 2012 Remaster etc without affecting songs with an actual hyphen
+#                         title = pattern.sub('', title)
+#                         title= re.sub(r'\([^]]*\)', '', title)
+#                         title = re.sub(r'\[[^]]*\]', '', title)
+#                         title = title.strip()
+#                         title = title.replace(" ", "+")
+#                         artist = artist.replace(" ", "+")
+#                         baseURL = "https://sing.whatever.social/search?q="
+#                         searchURL = f'{baseURL}{title}+{artist}'
+#                         r = requests.get(searchURL)
+#                         if(r is not None):
+#                             soup = BeautifulSoup(r.content, 'html5lib')
+#                         if(soup is not None):
+#                             songLink = soup.find('a', attrs = {'id':"search-item"}, href=True)['href']
+#                         if(songLink is not None):
+#                             songLink = baseURL[:28]+songLink
+#                             urls.append(songLink)
+#             urlmatrix.append(urls)
+#      return urlmatrix
 
-    
+# if __name__ == '__main__':
+#     # userFile = open("users.txt",'r')
+#     # Lines = userFile.readlines()
+#     # for line in Lines:
+#     #     playlist_info_2.main(line.strip())
+#     #urlmatrix= getUrls()
+#     #print(getUrls())
+#     p = Pool(4)
+#     result = 
+
+# def tester():
+#     lis = "Smooth Criminal - 2012 Remaster"
+#     pattern = re.compile('(- [^-]*)$')
+#     lis = pattern.sub('', lis)
+#     lis= re.sub(r'\([^]]*\)', '', lis)
+#     lis = re.sub(r'\[[^]]*\]', '', lis)
+#     lis = lis.strip()
+#     print(lis)
--- playlist_info_2.py
+++ playlist_info_2.py
@@ -86,9 +86,9 @@ def get_playlist_tracks(username,playlist_info):
   
     # Path
     path = os.path.join(parent_dir, directory)
-    if os.path.exists(f'{username}_playlists') == False :
+    if os.path.exists(f'Users/{username}_playlists') == False :
      os.mkdir(path)
-    shutil.move(play_id+'.txt', f'{username}_playlists/{play_id}.txt')
+    shutil.move(play_id+'.txt', f'Users/{username}_playlists/{play_id}.txt')
 
  
     return 0
