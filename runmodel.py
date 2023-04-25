8038c535b20764019ba9a1a3b52f0f56345f7201
JasonRen003 <Jasren003@gmail.com>
2023-04-25 18:56:53 -0500

    finished running model

--- runmodel.py
+++ runmodel.py
@@ -4,14 +4,13 @@ import os
 import pickle
 
 pickleDir = os.getcwd() + "\lyricPickles"
-#print(pickleDir)
 
 results = []
 for file in os.listdir(pickleDir):
     data = loadPickler.main(pickleDir + "\\" + file)
     data = [i for i in data if i != '']
     results.append(model.process_user(data))
-with open("modelresults") as f:
-    pickle.dump(results,f)
+outFile = open('modelresults', 'wb')
+pickle.dump(results,outFile)
 
 
