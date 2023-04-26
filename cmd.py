cfb8ef9a46b68b3809d3e95fbebfad114576598a
sahanagana <sahaana.ganapathy@gmail.com>
2023-04-26 16:04:52 -0500

    debug cmd line

--- cmd.py
+++ cmd.py
@@ -1,12 +1,13 @@
 import argparse
 import api
+import llm.model
 
 parser = argparse.ArgumentParser(description='generate ML tags from a song')
 parser.add_argument('-t', '--title', help = 'title of song', required= True)
 parser.add_argument('-a', '--artist', help = 'artist of song', required= True)
 args = parser.parse_args()        
 
-lyrics = api.getLyrics(title, artist)
+lyrics = api.getLyrics(args.title, args.artist)
 results= model.gen_tags(lyrics, 8)
 
 print('/n results: ' + results) 
