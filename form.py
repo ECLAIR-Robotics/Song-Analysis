48ee26a09223a7d3537ab6f18ce1d752ffb949b4
sahanagana <sahaana.ganapathy@gmail.com>
2023-04-26 13:32:37 -0500

    debug form

--- form.py
+++ form.py
@@ -1,14 +1,20 @@
-import llm.model as model
-import cgi
-import api 
 
-form = cgi.FieldStorage()
-title =  form.getvalue('title')
-artist = form.getvalue('artist')
-lyrics = api.getLyrics(title, artist)
+from flask import Flask, request, render_template,jsonifyapp = Flask(__name__)def do_something(text1,text2):
+    lyrics = api.getLyrics(title, artist)   results = text1 + text2
+    results= model.gen_tags(lyrics, 8) 
+    return results@app.route('/')
 
-results= model.gen_tags(lyrics, 8)
+def home():
+    return render_template('home.html')@app.route('/join', methods=['GET','POST'])
 
-htmltext = "Results: %s" % (results)
-htmlfile = open('index.html', 'a')
-file.write(htmltext)
+def my_form_post():
+    title = request.form['title']
+    word = request.args.get('title')
+    artist = request.form['artist']
+    results = do_something(title, artist)
+    result = {
+        "output": results
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
